8ac7e3a84c490f236a9714c502682f1c6c2284b2
rlaxoan <rlaxoan9327@jj.ac.kr>
2024-04-08 09:12:50 +0000

    add nonjucks

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ var express = require('express')
 // func express to save as variable
 var app = express()
 
+var nunjucks = require("nunjucks")
+
+app.set("view engine", "html")
+nunjucks.configure("./views", {express: app})
+
 // Open server with 3456
 app.listen(3456, function() {
     console.log("start! express server on port 3456")
@@ -11,13 +16,31 @@ app.listen(3456, function() {
 
 app.get('/', function(req, res){
     //res.send("<h3>ExpressJS 준비완료</h3>")
-    res.sendFile(__dirname+"/main.html")
+    //res.sendFile(__dirname+"/main.html")
+	res.render("main.html")
 })
 
 app.get('/main.html', function(req, res) {
 	res.sendFile(__dirname+"/main.html")
+
 })
 
 app.get('/another.html', function(req, res) {
 	res.sendFile(__dirname+"/another.html")
 })
+
+app.get('/download', function(req, res) {
+	//res.sendFile(__dirname+"/download.html")
+	const file = `${__dirname}/test.txt`;
+	res.download(file, function (err){
+		if (err) {
+			console.log("occured error : ", err.name)
+			console.log("messages : ", err.message)
+			res.end()
+		} else {
+			console.log("Success?")
+		}
+	})
+	res.end()
+})
+
